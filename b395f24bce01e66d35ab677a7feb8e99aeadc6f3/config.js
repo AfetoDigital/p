51372687adb5b6918f5ea94fcff7c7c016f3e686
P£0j&t0 <tiagolindo31_@hotmail.com>
2024-11-28 10:27:12 -0300

    feat: adding templates

---
+++ b395f24bce01e66d35ab677a7feb8e99aeadc6f3/config.js
@@ -0,0 +1,22 @@
+
+        // Configuração das estrelas
+        const memories = [
+            {
+                title: "Encontro",
+                text: "Primeiro encontro bla bla bla",
+                images: ["https://services.meteored.com/img/article/inteligencia-artificial-aprende-a-reconstruir-imagens-vistas-por-pessoas-ciencia-fotos-1679175318563_1280.jpg"],
+                color: "orange"
+            },
+            {
+                title: "Parque",
+                text: "Primeira vez no parque bla bla bla",
+                images: ["https://s2-techtudo.glbimg.com/YV466w-hUwF_dTQIHPj_sbg9pW4=/0x0:1536x864/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2024/P/F/9THicRSG2c2VTkIKVkAw/imagine.art-ai-2.png"],
+                color: "white"
+            },
+            {
+                title: "Ultimo dia",
+                text: "Ultimo dia bla bla bla bla",
+                images: ["https://s2-techtudo.glbimg.com/YV466w-hUwF_dTQIHPj_sbg9pW4=/0x0:1536x864/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2024/P/F/9THicRSG2c2VTkIKVkAw/imagine.art-ai-2.png", "https://services.meteored.com/img/article/inteligencia-artificial-aprende-a-reconstruir-imagens-vistas-por-pessoas-ciencia-fotos-1679175318563_1280.jpg"],
+                color: "white"
+            }
+        ];
