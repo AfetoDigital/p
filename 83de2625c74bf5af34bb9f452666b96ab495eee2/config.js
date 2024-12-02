95e1fd427662bab2d8b1664e8dec126529ebcf4c
s2-mylife <tiagolindo305_@hotmail.com>
2024-12-02 16:27:26 -0300

    fix name
    fixed woman name

--- 83de2625c74bf5af34bb9f452666b96ab495eee2/config.js
+++ 83de2625c74bf5af34bb9f452666b96ab495eee2/config.js
@@ -2,7 +2,7 @@
 
 let config = {
     "man": "Gustavo",
-    "woman": "Rau",
+    "woman": "Luana",
     "first_message": "Desde o instante em que nossos caminhos se cruzaram, tudo se tornou mais colorido. Cada momento ao seu lado é uma eternidade de felicidade. Em cada sorriso seu, eu encontro paz, e em cada olhar, vejo a promessa de um amor que cresce a cada dia. Eu te amo mais do que as palavras podem dizer, e sei que ao seu lado, o futuro é ainda mais brilhante.",
     "second_message": "Eu sou imensamente grato(a) por ter você em minha vida. Que cada dia ao seu lado seja uma celebração do nosso amor eterno. Te amo! <span class='heart'>❤️</span>",
   };
